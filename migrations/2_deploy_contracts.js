2679154e4db69c225cd638b3242e5d969c14718b
I-aM-FeNi <fenimabusa@gmail.com>
2023-08-30 23:36:59 +0200

    UPDATED

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,16 +1,23 @@
-const { ethers, upgrades } = require("hardhat");
-
 async function main() {
-  const HelloWorld = await ethers.getContractFactory("HelloWorld");
-  const helloWorld = await HelloWorld.deploy();
-  await helloWorld.deployed();
 
-  console.log("HelloWorld deployed to:", helloWorld.address);
+    const [deployer] = await ethers.getSigners();
+
+    console.log(
+    "Deploying contracts with the account:",
+    deployer.address
+    );
+
+    console.log("Account balance:", (await deployer.getBalance()).toString());
+
+    const HelloWorld = await ethers.getContractFactory("HelloWorld");
+    const contract = await HelloWorld.deploy();
+
+    console.log("Contract deployed at:", contract.address);
 }
 
 main()
   .then(() => process.exit(0))
-  .catch((error) => {
+  .catch(error => {
     console.error(error);
     process.exit(1);
   });
